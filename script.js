8903676097676eab6586e2a88f567640cff0459d
Balajisanthanam205 <152350664+Balajisanthanam205@users.noreply.github.com>
2024-09-09 20:32:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-document.getElementById('checkButton').addEventListener('click', function() {
-    // Get the value from the input field
-    const input = document.getElementById('valuesInput').value;
-    
-    // Convert the input string into an array of numbers
-    const values = input.split(',').map(value => parseFloat(value.trim()));
-    
-    // Define the condition
-    const conditionValue = 4.900;
 
-    // Check each value and show an alert if the condition is met
-    values.forEach(value => {
-        if (value > conditionValue) {
-            alert(`WARNING THERE IS A CRACK DETECTED!!!!!!`);
-        }
-    });
 
-    // If no value exceeds the condition, show a general message
-    if (values.every(value => value <= conditionValue)) {
-        alert('THE RAILWAY TRACK IS IN GOOD CONDITION');
-    }
-});
+
+
 
 
 
